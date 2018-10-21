3cf8b7616c361ef4567e207db312874dc6bea908
John3Kim <johnary_kim@hotmail.com>
2018-10-21 11:45:50 -0400

    Title Changes; siderInput changes; default graph change

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ library(shiny)
 shinyUI(fluidPage(
    
    # Application title
-   titlePanel("GDP Data of Cloud Computing Services and GDP of Canada"),
+   titlePanel("Information and communication technology contribution to GDP in Canada"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
@@ -12,12 +12,12 @@ shinyUI(fluidPage(
       sidebarPanel(
          sliderInput("yr_range", "Year range:",
                      min = 2007, max = 2018,
-                     value = c(2007,2010), 
+                     value = c(2007,2018), 
                      sep = "", 
                      ticks = FALSE),
          p(strong("Choose a dataset to display:")),
          checkboxInput(inputId="choose_ict", 
-                       label="Information and communication technology sector (Blue)", value = FALSE), 
+                       label="Information and communication technology sector (Blue)", value = TRUE), 
          checkboxInput(inputId="choose_icts", 
                        label="Information and communication technology, services (Red)", value = FALSE)
       ),
